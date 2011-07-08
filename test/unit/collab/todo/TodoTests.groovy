bca5d9549eea923c48b0c90943f8eeb7550a3f28
Gibran Montes <gibran@humanos.com.mx>
2011-07-08 14:25:52 -0500

    Correción de archivos git

--- test/unit/collab/todo/TodoTests.groovy
+++ test/unit/collab/todo/TodoTests.groovy
@@ -11,25 +11,28 @@ import org.junit.*
 @TestFor(Todo)
 class TodoTests {
 
-	void setUp() {
-		Todo.list()*.delete()
-	}
+    void setUp() {
+        Todo.list()*.delete()
+    }
 
-	void testPersist() {
-		new Todo(name: "1", createdDate: new Date(), priority: "", status: "").save()
-		new Todo(name: "2", createdDate: new Date(), priority: "", status: "").save()
-		new Todo(name: "3", createdDate: new Date(), priority: "", status: "").save()
-		new Todo(name: "4", createdDate: new Date(), priority: "", status: "").save()
-		new Todo(name: "5", createdDate: new Date(), priority: "", status: "").save()
+    void testPersist() {
+        new Todo(name: "1", createdDate: new Date(), priority: "", status: "").save()
+        new Todo(name: "2", createdDate: new Date(), priority: "", status: "").save()
+        new Todo(name: "3", createdDate: new Date(), priority: "", status: "").save()
+        new Todo(name: "4", createdDate: new Date(), priority: "", status: "").save()
+        new Todo(name: "5", createdDate: new Date(), priority: "", status: "").save()
 
-		assert 5 == Todo.count()
-		def actualTodo = Todo.findByName('1')
-		assert actualTodo
-		assert '1' == actualTodo.name
-	}
+        assert 5 == Todo.count()
+        def actualTodo = Todo.findByName('1')
+        assert actualTodo
+        assert '1' == actualTodo.name
+    }
 
-	void testToString() {
-		def todo = new Todo(name: "Pickup laundry")
-		assertToString(todo, "Pickup laundry")
-	}
+    void testToString() {
+        def todo = new Todo(name: "Pickup laundry")
+        assertToString(todo, "Pickup laundry")
+    }
+
+    void testSomething() {
+    }
 }
